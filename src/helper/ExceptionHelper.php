0bdb125a735d06ff5198542b323e555705cb93de
albert <63851587@qq.com>
2018-11-21 11:52:52 +0800

    update

---
+++ src/helper/ExceptionHelper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/21
+ * Time: 2:37
+ */
+
+namespace rabbit\helper;
+
+/**
+ * Class ExceptionHelper
+ * @package rabbit\helper
+ */
+class ExceptionHelper
+{
+    /**
+     * @param $exception
+     * @return array
+     */
+    public static function convertExceptionToArray($exception): array
+    {
+        $array = [
+            'name' => $exception instanceof Exception ? $exception->getName() : 'Exception',
+            'message' => $exception->getMessage(),
+            'code' => $exception->getCode(),
+        ];
+        if (ObjectFactory::get('debug', false)) {
+            $array['type'] = get_class($exception);
+            if (!$exception instanceof UserException) {
+                $array['file'] = $exception->getFile();
+                $array['line'] = $exception->getLine();
+                $array['stack-trace'] = explode("\n", $exception->getTraceAsString());
+            }
+        }
+        if (($prev = $exception->getPrevious()) !== null) {
+            $array['previous'] = self::convertExceptionToArray($prev);
+        }
+
+        return $array;
+    }
+}
