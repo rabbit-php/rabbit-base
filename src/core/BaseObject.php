a8a589fde52fef8c3bf1ee5454dc2261c5b36984
albert <63851587@qq.com>
2019-01-24 14:54:02 +0800

    update

--- src/core/BaseObject.php
+++ src/core/BaseObject.php
@@ -19,6 +19,10 @@ class BaseObject
      */
     public function __get($name)
     {
+        $getter = 'get' . $name;
+        if (method_exists($this, $getter)) {
+            return $this->$getter();
+        }
         return $this->$name;
     }
 
@@ -28,6 +32,11 @@ class BaseObject
      */
     public function __set($name, $value)
     {
+        $setter = 'set' . $name;
+        if (method_exists($this, $setter)) {
+            $this->$setter($value);
+            return;
+        }
         $this->$name = $value;
     }
 }
