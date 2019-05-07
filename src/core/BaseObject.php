52c1e6e2af372b75d72bbbb1f59a8e212a7dc1e2
albert <63851587@qq.com>
2019-05-08 02:52:22 +0800

    update

--- src/core/BaseObject.php
+++ src/core/BaseObject.php
@@ -8,6 +8,8 @@
 
 namespace rabbit\core;
 
+use yii\exceptions\InvalidCallException;
+
 /**
  * Class BaseObject
  * @package rabbit\core
@@ -38,5 +40,70 @@ class BaseObject
             return;
         }
         $this->$name = $value;
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        $getter = 'get' . $name;
+        if (method_exists($this, $getter)) {
+            return $this->$getter() !== null;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $name
+     */
+    public function __unset($name)
+    {
+        $setter = 'set' . $name;
+        if (method_exists($this, $setter)) {
+            $this->$setter(null);
+        }
+        $this->$name = null;
+    }
+
+    /**
+     * @param $name
+     * @param bool $checkVars
+     * @return bool
+     */
+    public function hasProperty($name, $checkVars = true)
+    {
+        return $this->canGetProperty($name, $checkVars) || $this->canSetProperty($name, false);
+    }
+
+    /**
+     * @param $name
+     * @param bool $checkVars
+     * @return bool
+     */
+    public function canGetProperty($name, $checkVars = true)
+    {
+        return method_exists($this, 'get' . $name) || $checkVars && property_exists($this, $name);
+    }
+
+    /**
+     * @param $name
+     * @param bool $checkVars
+     * @return bool
+     */
+    public function canSetProperty($name, $checkVars = true)
+    {
+        return method_exists($this, 'set' . $name) || $checkVars && property_exists($this, $name);
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function hasMethod($name)
+    {
+        return method_exists($this, $name);
     }
 }
