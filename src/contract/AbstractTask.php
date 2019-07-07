c233036f8857f5395935b3d511fe7de96c0b906c
albert <63851587@qq.com>
2019-07-07 22:54:34 +0800

    update

--- src/contract/AbstractTask.php
+++ src/contract/AbstractTask.php
@@ -11,6 +11,22 @@ use rabbit\App;
  */
 abstract class AbstractTask
 {
+    /** @var array */
+    protected $taskList = [];
+    /** @var string */
+    protected $logKey = 'Task';
+    /** @var string */
+    protected $taskName;
+
+    /**
+     * AbstractTask constructor.
+     * @param array $taskList
+     */
+    public function __construct(string $name = null)
+    {
+        $this->taskName = $name ?? uniqid();
+    }
+
     /**
      * @param int $task_id
      * @param int $from_id
@@ -24,16 +40,30 @@ abstract class AbstractTask
      * @param int $task_id
      * @param string $data
      */
-    abstract public function finish(\Swoole\Server $serv, int $task_id, string $data): void;
+    public function finish(\Swoole\Server $serv, int $task_id, string $data): void
+    {
+    }
+
 
     /**
-     * @param array $tasks
      * @param float $timeout
      * @return array
      */
-    public static function taskCo(array $tasks, float $timeout = 0.5): array
+    public function start(float $timeout = 0.5): array
     {
-        $result = App::getServer()->taskCo($tasks, $timeout);
+        App::info('Task' . " $this->taskName " . 'start count=' . count($this->taskList), $this->logKey);
+        $result = App::getServer()->taskCo($this->taskList, $timeout);
+        App::info('Task' . " $this->taskName " . 'finish!', $this->logKey);
         return is_array($result) ? $result : [$result];
+    }
+
+    /**
+     * @param $task
+     * @return AbstractTask
+     */
+    public function addTask($task): self
+    {
+        $this->taskList[] = $task;
+        return $this;
     }
 }
