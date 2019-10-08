82b97eb73679ff1ce986c8840d08f9cb55f1e9aa
albert <63851587@qq.com>
2019-10-08 18:20:45 +0800

    update

--- src/helper/ExceptionHelper.php
+++ src/helper/ExceptionHelper.php
@@ -9,8 +9,6 @@
 namespace rabbit\helper;
 
 use rabbit\core\Exception;
-use rabbit\core\ObjectFactory;
-use rabbit\core\UserException;
 
 /**
  * Class ExceptionHelper
@@ -28,15 +26,11 @@ class ExceptionHelper
             'name' => $exception instanceof Exception ? $exception->getName() : 'Exception',
             'message' => $exception->getMessage(),
             'code' => $exception->getCode(),
+            'type' => get_class($exception),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+            'stack-trace' => explode("\n", $exception->getTraceAsString()),
         ];
-        if (ObjectFactory::get('debug', false)) {
-            $array['type'] = get_class($exception);
-            if (!$exception instanceof UserException) {
-                $array['file'] = $exception->getFile();
-                $array['line'] = $exception->getLine();
-                $array['stack-trace'] = explode("\n", $exception->getTraceAsString());
-            }
-        }
         if (($prev = $exception->getPrevious()) !== null) {
             $array['previous'] = self::convertExceptionToArray($prev);
         }
