823716abd8e6ca05b7faec0e384e003f933973ee
albert <63851587@qq.com>
2019-10-10 19:22:10 +0800

    add trace depth

--- src/helper/ExceptionHelper.php
+++ src/helper/ExceptionHelper.php
@@ -17,11 +17,16 @@ use rabbit\core\Exception;
 class ExceptionHelper
 {
     /**
-     * @param $exception
+     * @param \Throwable $exception
      * @return array
+     * @throws \Exception
      */
-    public static function convertExceptionToArray($exception): array
+    public static function convertExceptionToArray(\Throwable $exception): array
     {
+        $trace = explode("\n", $exception->getTraceAsString());
+        $count = count($trace);
+        $depth = getDI('debug_depth', false, 0);
+        $count < $depth && $depth = $count;
         $array = [
             'name' => $exception instanceof Exception ? $exception->getName() : 'Exception',
             'message' => $exception->getMessage(),
@@ -29,7 +34,7 @@ class ExceptionHelper
             'type' => get_class($exception),
             'file' => $exception->getFile(),
             'line' => $exception->getLine(),
-            'stack-trace' => explode("\n", $exception->getTraceAsString()),
+            'stack-trace' => $depth === 0 ? $depth : array_slice($trace, 0, $depth),
         ];
         if (($prev = $exception->getPrevious()) !== null) {
             $array['previous'] = self::convertExceptionToArray($prev);
