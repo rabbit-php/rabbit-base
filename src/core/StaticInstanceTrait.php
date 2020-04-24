3e658322de7d35842783a7396cf3ca8a9271efa5
albert <amuluowin@163.com>
2020-04-24 14:20:41 +0800

    fix bug

--- src/core/StaticInstanceTrait.php
+++ src/core/StaticInstanceTrait.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace rabbit\core;
 
 /**
  * Trait StaticInstanceTrait
- * @package yii\base
+ * @package rabbit\core
  */
 trait StaticInstanceTrait
 {
@@ -28,7 +23,7 @@ trait StaticInstanceTrait
     {
         $className = get_called_class();
         if ($refresh || !isset(self::$_instances[$className])) {
-            self::$_instances[$className] = Yii::createObject($className);
+            self::$_instances[$className] = ObjectFactory::createObject($className);
         }
         return self::$_instances[$className];
     }
