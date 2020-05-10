7c5b509bbebc6bf2100ab66a13d4acb637d94245
albert <amuluowin@163.com>
2020-05-11 00:51:52 +0800

    add corunner

---
+++ src/core/ChanRun.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace rabbit\core;
+
+use Co\Channel;
+use rabbit\App;
+use rabbit\core\Exception;
+
+/**
+ * Class ChanRun
+ * @package rabbit\core
+ */
+class ChanRun
+{
+    /** @var Channel */
+    protected $channel;
+    /** @var bool */
+    protected $running = false;
+    /** @var callable */
+    protected $callback;
+
+    /**
+     * ChanRun constructor.
+     * @param callable $callback
+     * @param int|null $size
+     */
+    public function __construct(callable $callback, int $size = null)
+    {
+        $this->channel = new Channel($size);
+        $this->callback = $callback;
+    }
+
+    /**
+     * @param $data
+     * @return bool
+     */
+    public function push($data): bool
+    {
+        return $this->channel->push($data);
+    }
+
+    /**
+     * @param callable|null $call
+     * @return int
+     * @throws Exception
+     * @throws \Exception
+     */
+    public function process(?callable $call = null): int
+    {
+        if ($this->running) {
+            $msg = "process already running";
+            App::warning($msg);
+            throw new Exception($msg);
+        }
+        $this->running = true;
+        return rgo(function () use ($call) {
+            while ($this->running) {
+                $data = $this->channel->pop();
+                rgo(function () use ($call, $data) {
+                    $result = call_user_func($this->callback, $data);
+                    if (is_callable($call)) {
+                        call_user_func($call, $result);
+                    }
+                });
+            }
+        });
+    }
+}
