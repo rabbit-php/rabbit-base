8b6c23e1671f0b0d3995cb0ccd1ef794ba0f5723
albert <amuluowin@163.com>
2020-07-11 19:40:00 +0800

    update xml

--- src/helper/XmlHelper.php
+++ src/helper/XmlHelper.php
@@ -1,10 +1,17 @@
 <?php
 declare(strict_types=1);
+
 namespace Rabbit\Base\Helper;
+
+use DOMDocument;
+use DOMElement;
+use DOMText;
+use Rabbit\Base\Contract\ArrayAble;
+use Traversable;
 
 /**
  * Class XmlHelper
- * @package rabbit\helper
+ * @package Rabbit\Base\Helper
  */
 class XmlHelper
 {
@@ -99,5 +106,74 @@ class XmlHelper
     protected static function characterDataReplace(string $string): string
     {
         return sprintf('<![CDATA[%s]]>', $string);
+    }
+
+
+    /**
+     * @param array $data
+     * @param string $rootTag
+     * @param string $itemTag
+     * @param string $version
+     * @param string $encoding
+     * @return mixed
+     */
+    public static function format(?array $data, string $rootTag = 'root', string $itemTag = 'item', string $version = '1.0', string $encoding = 'UTF-8'): string
+    {
+        $content = '';
+        if ($data !== null) {
+            $dom = new DOMDocument($version, $encoding);
+            $root = new DOMElement($rootTag);
+            $dom->appendChild($root);
+            self::buildXml($root, $data, $itemTag);
+            $content = $dom->saveXML();
+        }
+
+        return $content;
+    }
+
+    /**
+     * @param DOMElement $element
+     * @param mixed $data
+     * @param string $itemTag
+     * @return void
+     */
+    protected static function buildXml(DOMElement $element, $data, string $itemTag = 'item'): void
+    {
+        if (is_array($data) ||
+            ($data instanceof Traversable && !$data instanceof Arrayable)
+        ) {
+            if ($data) {
+                foreach ($data as $name => $value) {
+                    if (is_int($name) && is_object($value)) {
+                        self::buildXml($element, $value);
+                    } elseif (is_array($value) || is_object($value)) {
+                        $child = new DOMElement(is_int($name) ? $itemTag : $name);
+                        $element->appendChild($child);
+                        self::buildXml($child, $value);
+                    } else {
+                        $child = new DOMElement(is_int($name) ? $itemTag : $name);
+                        $element->appendChild($child);
+                        $child->appendChild(new DOMText((string)$value));
+                    }
+                }
+            } else {
+                $element->appendChild(new DOMText((string)null));
+            }
+
+        } elseif (is_object($data)) {
+            $child = new DOMElement(StringHelper::basename(get_class($data)));
+            $element->appendChild($child);
+            if ($data instanceof Arrayable) {
+                self::buildXml($child, $data->toArray());
+            } else {
+                $array = [];
+                foreach ($data as $name => $value) {
+                    $array[$name] = $value;
+                }
+                self::buildXml($child, $array);
+            }
+        } else {
+            $element->appendChild(new DOMText((string)$data));
+        }
     }
 }
