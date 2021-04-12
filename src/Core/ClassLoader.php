636fcd835b5b0c99eba18bead105e7e34880954f
Albert <amuluowin@163.com>
2021-04-12 15:42:08 +0800

    add preemptive support

---
+++ src/Core/ClassLoader.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rabbit\Base\Core;
+
+use Composer\Autoload\ClassLoader as AutoloadClassLoader;
+
+class ClassLoader
+{
+    private AutoloadClassLoader $loader;
+
+    private static $isInit = false;
+
+    public function __construct(AutoloadClassLoader $loader)
+    {
+        $this->loader = $loader;
+    }
+
+    public function loadClass($class)
+    {
+        \Co::disableScheduler();
+        $this->loader->loadClass($class);
+        \Co::enableScheduler();
+    }
+
+    public static function PreemptiveLoader(AutoloadClassLoader $loader): void
+    {
+        if (!self::$isInit) {
+            $loader->unregister();
+            spl_autoload_register(array(new static($loader), 'loadClass'));
+        }
+    }
+}
